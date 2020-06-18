03f3d1b77cde009fe80f779918f9952774c7d357
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-06-18 19:03:17 +0200

    get products

---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+function MyApp({ Component, pageProps }) {
+  return (
+    <main>
+      <header>
+        <nav>
+          <p>AQUÍ IRA LA BARRA DE NAVEGACION</p>
+        </nav>
+      </header>
+      <Component {...pageProps} />
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </main>
+  );
+}
+
+
+
+
+export default MyApp;
