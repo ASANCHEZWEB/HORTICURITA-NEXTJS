318d93ac1130cb27910c61f08c3dc54cf943a295
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-07-11 14:49:24 +0200

    quitar fuentes de google

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Miltonian+Tattoo|Pacifico|Montserrat&display=swap"
-        />
-      </Head>
+      
 
       <header>
         <NavbarComponent />
