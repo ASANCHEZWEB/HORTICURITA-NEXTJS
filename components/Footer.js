ddf6c005216962e29fe8f8f168606535749dfa1c
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-07-13 16:49:49 +0200

    añadido footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,17 +5,214 @@ class Footer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      imagenTelefono: "",
+      imagenWhatsapp: "",
+      imagenChat: "",
+      iconoInstagram: "",
+      iconoFacebook: "",
+      iconoTwitter: "",
+    };
   }
 
+  componentDidMount() {
+    this.setState({
+      imagenTelefono:
+        "https://res.cloudinary.com/dfsni6m2x/image/upload/c_scale,h_33,q_10,w_33/v1594578688/iconosHorticurita/telefono_ibn7ia.png",
+      imagenWhatsapp:
+        "https://res.cloudinary.com/dfsni6m2x/image/upload/c_scale,h_33,q_10,w_33/v1594578040/iconosHorticurita/whatsapp-64_wdhynx.png",
+      imagenChat:
+        "https://res.cloudinary.com/dfsni6m2x/image/upload/c_scale,h_33,q_10,w_33/v1594578971/iconosHorticurita/icono-charla_znjdc4.png",
+      iconoInstagram:
+        "https://res.cloudinary.com/dfsni6m2x/image/upload/c_scale,h_33,q_10,w_33/v1594579228/iconosHorticurita/icono-instagram_ir06gu.png",
+      iconoFacebook:
+        "https://res.cloudinary.com/dfsni6m2x/image/upload/c_scale,h_33,q_10,w_33/v1594579351/iconosHorticurita/icono_facebook_bxuabc.png",
+      iconoTwitter:
+        "https://res.cloudinary.com/dfsni6m2x/image/upload/c_scale,h_33,q_10,w_33/v1594585327/iconosHorticurita/icono_twitter_jvqpup.png",
+    });
+  }
   render() {
     return (
-      
+      <>
         <footer>
-      este es el footer
-      </footer>
-      
-      
+          <div>
+            <h5>Contacto</h5>
+            <a href="tel:+34612345678">
+              <img src={this.state.imagenTelefono} alt="icono teléfono" />
+              Teléfono
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=34612345678">
+              <img src={this.state.imagenWhatsapp} alt="icono whatsapp" />
+              Whatsapp
+            </a>
+            <button>
+              <img src={this.state.imagenChat} alt="icono chat en línea" /> Chat
+              en línea
+            </button>
+          </div>
+          <div>
+            <h5>Categorías</h5>
+            <ul>
+              <li>
+                <a>Frutas</a>
+              </li>
+              <li>
+                <a>Verduras y hortalizas</a>
+              </li>
+              <li>
+                <a>Legumbres</a>
+              </li>
+              <li>
+                <a>Frutos secos</a>
+              </li>
+              <li>
+                <a>Encurtidos</a>
+              </li>
+              <li>
+                <a>Especias</a>
+              </li>
+              <li>
+                <a>Miel</a>
+              </li>
+              <li>
+                <a>Dulces</a>
+              </li>
+              <li>
+                <a>Galletas y cereales</a>
+              </li>
+              <li>
+                <a>Aperitivos Snack</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Información</h5>
+            <a>Condiciones generales</a>
+            <a>Política de privacidad</a>
+            <a>Política de Cookies</a>
+            <a>Condiciones de compra</a>
+            <a>Calidad</a>
+          </div>
+          <div>
+            <h5>Redes sociales</h5>
+            <a target="_blank" href="https://www.instagram.com/horticurita.es/">
+              <img src={this.state.iconoInstagram} alt="icono instagram" />
+              Instagram
+            </a>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/Horticurita-110947830690065"
+            >
+              <img src={this.state.iconoFacebook} alt="icono facebook" />
+              Facebook
+            </a>
+            <a target="_blank" href="https://twitter.com/horticurita.es">
+              <img src={this.state.iconoTwitter} alt="icono facebook" />
+              Twitter
+            </a>
+          </div>
+
+          <div className="footerCopyright">
+            <hr></hr>
+            2020 © Copyright Horticurita | <a>Desarrollado por Alberto S.</a>
+          </div>
+        </footer>
+        <style jsx>{`
+          footer {
+            background-color: #2ecc71;
+            display: flex;
+            justify-content: space-evenly;
+            font-family: Montserrat;
+            flex-wrap: wrap;
+            margin-top: 20px;
+          }
+
+          footer > div {
+            padding: 38px 15px 38px 15px;
+          }
+
+          footer > div > h5 {
+            font-size: 2em;
+            color: white;
+            margin-top: 1px;
+            margin-bottom: 19px;
+          }
+
+          footer > div > ul > li {
+            margin-top: 5px;
+            color: white;
+          }
+
+          footer > div > button {
+            margin: 0px;
+            background: none;
+            border: none;
+            display: flex;
+            align-items: center;
+            color: white;
+            font-family: montserrat;
+            font-size: 14px;
+            outline: none;
+          }
+
+          footer > div:nth-child(1) {
+            display: flex;
+            flex-direction: column;
+          }
+          footer > div:nth-child(1) > button {
+            padding: 0px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+          }
+          footer > div:nth-child(1) > a {
+            margin-top: 10px;
+            margin-bottom: 10px;
+            display: flex;
+            align-items: center;
+          }
+
+          footer > div:nth-child(3) {
+            display: flex;
+            flex-direction: column;
+          }
+
+          footer > div:nth-child(3) > a {
+            margin-bottom: 5px;
+          }
+
+          footer > div:nth-child(4) {
+            display: flex;
+            flex-direction: column;
+          }
+
+          footer > div:nth-child(4) > a {
+            margin-top: 10px;
+            margin-bottom: 10px;
+            display: flex;
+            align-items: center;
+          }
+
+          footer > div > a {
+            text-decoration: none;
+            color: white;
+          }
+
+          .footerCopyright {
+            width: 100%;
+            text-align: center;
+            color: white;
+          }
+
+          .footerCopyright > hr {
+            background-color: #67f572;
+            margin-bottom: 1px;
+            border-radius: 10px;
+            max-width: 626px;
+            margin: 0 auto;
+            border-color: transparent;
+          }
+        `}</style>
+      </>
     );
   }
 }
