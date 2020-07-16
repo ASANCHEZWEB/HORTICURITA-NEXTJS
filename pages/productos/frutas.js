a1c26e03cb16ddee1cc7f13128d9e05c93e52432
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-07-16 18:13:38 +0200

    correccion head

--- pages/productos/frutas.js
+++ pages/productos/frutas.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Head from "next/link";
+import Head from "next/head";
 import { getLocalData } from "../../components/getLocalStorage";
 
 class TodaFruta extends React.Component {
