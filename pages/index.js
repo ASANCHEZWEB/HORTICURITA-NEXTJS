e220c14f5cf3d13fe5146850d144617a118617c6
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-07-23 02:54:58 +0200

    prueba con _document

--- pages/index.js
+++ pages/index.js
@@ -7,52 +7,12 @@ class Home extends React.Component {
     this.state = {};
   }
 
-  tagManager() {
-    if (!document.querySelector(".tagManager")) {
-      let element = document.createElement("script");
-      element.setAttribute("class", "tagManager");
-      element.setAttribute("async", "");
-      element.setAttribute("src","https://www.googletagmanager.com/gtag/js?id=UA-169048148-3");
-      document.querySelector("head").appendChild(element);
-    }else{
-      document.querySelector('.tagManager').setAttribute('src','https://www.googletagmanager.com/gtag/js?id=UA-169048148-3')
-    }
-    if (!document.querySelector("scriptTagManager")) {
-      let anaTag = document.createElement("script");
-      anaTag.setAttribute("class", "scriptTagManager");
-      anaTag.innerHTML = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-169048148-3');
-`;
-      document.querySelector("head").appendChild(anaTag);
-    }else{
-      document.querySelector('scriptTagManager').innerHTML=`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date()) ; 
-      gtag('config', 'UA-169048148-3')
-    `
-    }
-
-
-
-
-
-  }
+ 
 
   componentDidMount() {
-    this.tagManager();
+    
 
-    //   let anaTag = document.createElement("script");
-    //   anaTag.setAttribute('class','scriptTagManager');
-    //   anaTag.innerHTML = `
-    // window.dataLayer = window.dataLayer || [];
-    // function gtag(){dataLayer.push(arguments);}
-    // gtag('js', new Date());
-    // gtag('config', 'UA-169048148-3');
-    // `;
+  
   }
 
   render() {
