0aa0a2d1360bfe4569815b84232e19a0428d4e22
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-09-23 23:48:27 +0200

    terminado hasta aperitivos snack , falta aceite y queso

--- pages/productos/legumbres/[id].js
+++ pages/productos/legumbres/[id].js
@@ -1,34 +1,254 @@
-
+// pages/posts/[id].js
 import React from "react";
+import axios from 'axios';
 
 class Legumbre extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
-    this.objectData = props.objectProp;
+    this.myVar = "";
+    this.myFunction = () => {
+      this.myVar = setTimeout(function () {
+        document.querySelector(".addCartContainer").innerHTML = "";
+        document.querySelector(".addCartContainer").style.display = "none";
+      }, 5000);
+    };
+    this.myStopFunction = () => {
+      clearTimeout(this.myVar);
+    };
+    this.state = {
+           _id: props.objectProp._id,
+            name: props.objectProp.name,
+            price: props.objectProp.price,
+            description: props.objectProp.description,
+            category:props.objectProp.category,
+            subcategory:props.objectProp.subcategory,
+            generalcategory: props.objectProp.generalcategory,
+            imageUrl: props.objectProp.imageUrl,
+            imageAlt: props.objectProp.imageAlt,
+            type: props.objectProp.type,
+            stock: props.objectProp.stock,
+            urlRoute: props.objectProp.urlRoute,
+            imageName: props.objectProp.imageName,
+            added: props.objectProp.added,
+            origin: props.objectProp.origin,
+            minimunQty:props.objectProp.minimunQty
+    };
+    
+    this.addQty=this.addQty.bind(this);
+    this.restQty=this.restQty.bind(this);
+    this.buscarProductoLS=this.buscarProductoLS.bind(this);
+    this.restarCuadro=this.restarCuadro.bind(this);
+    this.mostrarCuadro=this.mostrarCuadro.bind(this);
   }
+
+  restarCuadro = (product) => {
+    let tipo = "";
+    if (product.type == "kilogramos") {
+      tipo = "0.5 Kg";
+    } else {
+      tipo = "1 Ud";
+    }
+    if (document.querySelector(".addCartContainer")) {
+      //asignamos html a la etiqueta
+      document.querySelector(".addCartContainer").innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Eliminado ${tipo} de ${product.name} del carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    } else {
+      //asignamos html a la etiqueta
+      let elementDiv = document.createElement("div");
+      elementDiv.setAttribute("class", "addCartContainer");
+      elementDiv.innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Eliminado ${tipo} de ${product.name} del carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector("body").appendChild(elementDiv);
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    }
+  };
+  mostrarCuadro(product) {
+    let tipo = "";
+    if (product.type == "kilogramos") {
+      tipo = "0.5 Kg";
+    } else {
+      tipo = "1 Ud";
+    }
+    if (document.querySelector(".addCartContainer")) {
+      //asignamos html a la etiqueta
+      document.querySelector(".addCartContainer").innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Añadido ${tipo} de ${product.name} al carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    } else {
+      //asignamos html a la etiqueta
+      let elementDiv = document.createElement("div");
+      elementDiv.setAttribute("class", "addCartContainer");
+      elementDiv.innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Añadido ${tipo} de ${product.name} al carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector("body").appendChild(elementDiv);
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    }
+  }
+
+
+restQty(product) {
+   // si no esta creado el array del carrito lo creamos vacío
+   if(localStorage.getItem('cartProducts')==null){
+    localStorage.setItem("cartProducts","[]");
+  }
+ let buscarProducto = [...JSON.parse(localStorage.getItem('cartProducts')).filter(producto => {
+    return producto._id === product._id
+  })];
+
+  if(buscarProducto.length!==0){
+   buscarProducto[0].added--
+   if(buscarProducto[0].added<1){
+    let actualizarCarro= [...JSON.parse(localStorage.getItem('cartProducts'))].filter(element=>{
+      element._id!==product._id
+    });
+    localStorage.setItem("cartProducts",JSON.stringify(actualizarCarro))
+    this.setState({added:0})
+   }else{
+    let actualizarCarro= [...JSON.parse(localStorage.getItem('cartProducts'))].map(element=>{
+      if(element._id==product._id){
+        return buscarProducto[0]
+      }else{
+        return element
+      }
+    });
+    localStorage.setItem("cartProducts",JSON.stringify(actualizarCarro))
+    
+   }
+
+this.restarCuadro(product)
+
+  }
+ this.buscarProductoLS()
+
+}
+
+
+ addQty(product) {
+   // si no esta creado el array del carrito lo creamos vacío
+  if(localStorage.getItem('cartProducts')==null){
+    localStorage.setItem("cartProducts","[]");
+  }
+  //comprobamos si el producto tiene stock y añadimos o no
+  if(this.state.stock=="si"){
+//buscamos el producto para ver si esta añadido
+let arrayCarrito = [...JSON.parse(localStorage.getItem('cartProducts')).filter(producto => {
+  return producto._id === product._id
+  })]
+//si no se encuenta en el carro lo añadimos tal cual esta en el state pero con added + 1
+if(arrayCarrito.length==0){
+  let carritoArray= [...JSON.parse(localStorage.getItem('cartProducts'))];
+   product.added++;
+   carritoArray.push(product);
+   localStorage.setItem("cartProducts",JSON.stringify(carritoArray))
+}else{
+  let actualizarCarro= [...JSON.parse(localStorage.getItem('cartProducts'))].map(element=>{
+    if(element._id==product._id){
+      element.added++
+      return element
+    }else{
+      return element
+    }
+  });
+  localStorage.setItem("cartProducts",JSON.stringify(actualizarCarro))
+}
+this.mostrarCuadro(product);
+
+//aqui va la llamada ajax de que se ha metido al carro
+axios.post("https://gestorhorticurita.herokuapp.com/api/addedCart/", {
+  id: product._id,
+});
+  }
+  this.buscarProductoLS()
+ }
+
+
+
+buscarProductoLS() {
+  if (localStorage.getItem('cartProducts') !== null) {
+    let buscarProducto = [...JSON.parse(localStorage.getItem('cartProducts')).filter(producto => {
+      return producto._id === this.state._id
+    })];
+    if (buscarProducto.length !== 0) {
+      this.setState({added: buscarProducto[0].added})
+    }
+  }
+}
+
+componentDidMount(){
+this.buscarProductoLS()
+}
+
+
+
+componentWillUnmount(){
+  this.myStopFunction();
+  if(document.querySelector(".addCartContainer")){
+        document.querySelector(".addCartContainer").innerHTML = "";
+        document.querySelector(".addCartContainer").style.display = "none";
+  }
+}
 
   render() {
     return (
       <>
         <div className="containerDetail">
           <div>
-            <img
-              src={this.objectData.imageUrl}
-              alt={this.objectData.imageAlt}
-            />
+                   <picture>
+                      <source
+                        media="(max-width: 767.98px)"
+                        srcSet={`/141pxImages/${this.state.imageName}`}/>
+                      <source
+                        media="(max-width: 1199.98px) and (min-width: 768px)"
+                        srcSet={`/174pxImages/${this.state.imageName}`}
+                      />
+                      <source
+                        media="(min-width: 1200px)"
+                        srcSet={`/270pxImages/${this.state.imageName}`}
+                      />
+                      <img
+                        src={`/141pxImages/${this.state.imageName}`}
+                        alt={this.state.imageAlt}
+                      />
+                    </picture>
           </div>
-
           <div>
-            <h1>{this.objectData.name}</h1>
+            <h1>{this.state.name}</h1>
             <span>
-              {this.objectData.price} /
-              {this.objectData.type === "kilogramos" ? "Kg" : "Ud"}
+              {this.state.price} €/
+              {this.state.type === "kilogramos" ? "Kg" : "Ud"}
             </span>
-            <p>Origen: España</p>
+            <p>Origen: {this.state.origin}</p>
 
-            {this.objectData.stock === "si" ? (
+            {this.state.stock === "si" ? (
               <p>
                 <img
                   src="https://res.cloudinary.com/dfsni6m2x/image/upload/v1593730373/iconosHorticurita/icono_disponible_a932xs.png"
@@ -47,11 +267,11 @@ class Legumbre extends React.Component {
             )}
 
             <div className="buttonsCart">
-              <button type="button" className="buttonIzq">
+              <button type="button" className="buttonIzq" onClick={() => this.restQty(this.state)}>
                 -
               </button>
-              <input name="inputProduct" className="inputButtons" value="0" />
-              <button type="button" className="buttonDer">
+              <span className="inputButtons">{this.state.type=="kilogramos"? this.state.added/2:this.state.added}</span>
+              <button type="button" className="buttonDer" onClick={() => this.addQty(this.state)}>
                 +
               </button>
             </div>
@@ -62,10 +282,22 @@ class Legumbre extends React.Component {
           <hr></hr>
           <span>DESCRIPCIÓN</span>
           <hr></hr>
-          <p>{this.objectData.description}</p>
+          <p>{this.state.description}</p>
         </div>
 
         <style jsx>{`
+        .inputButtons {
+              max-width: 90px;
+              text-align: center;
+              width: 70px;
+              padding: 6px;
+              border-width: thin;
+              border-left: none rgb(255, 235, 0);
+              border-right: none rgb(255, 235, 0);
+              border-color: rgb(255, 235, 0);
+              border-top: 1px solid rgb(255, 235, 0);
+              border-bottom: 1px solid rgb(255, 235, 0);
+            }
           @media (max-width: 575.98px) {
             .containerDetail {
               display: flex;
@@ -126,16 +358,6 @@ class Legumbre extends React.Component {
               text-decoration: none;
               outline: none;
               touch-action: manipulation;
-            }
-
-            .inputButtons {
-              max-width: 90px;
-              text-align: center;
-              border-width: thin;
-              border-left: none rgb(255, 235, 0);
-              border-right: none rgb(255, 235, 0);
-              border-color: rgb(255, 235, 0);
-              border-top: 1px solid rgb(255, 235, 0);
             }
 
             .buttonDer {
@@ -245,15 +467,7 @@ class Legumbre extends React.Component {
               touch-action: manipulation;
             }
 
-            .inputButtons {
-              max-width: 90px;
-              text-align: center;
-              border-width: thin;
-              border-left: none rgb(255, 235, 0);
-              border-right: none rgb(255, 235, 0);
-              border-color: rgb(255, 235, 0);
-              border-top: 1px solid rgb(255, 235, 0);
-            }
+           
 
             .buttonDer {
               border-radius: 0px 15px 15px 0px;
@@ -360,15 +574,7 @@ class Legumbre extends React.Component {
               touch-action: manipulation;
             }
 
-            .inputButtons {
-              max-width: 90px;
-              text-align: center;
-              border-width: thin;
-              border-left: none rgb(255, 235, 0);
-              border-right: none rgb(255, 235, 0);
-              border-color: rgb(255, 235, 0);
-              border-top: 1px solid rgb(255, 235, 0);
-            }
+           
 
             .buttonDer {
               border-radius: 0px 15px 15px 0px;
@@ -475,15 +681,8 @@ class Legumbre extends React.Component {
               touch-action: manipulation;
             }
 
-            .inputButtons {
-              max-width: 90px;
-              text-align: center;
-              border-width: thin;
-              border-left: none rgb(255, 235, 0);
-              border-right: none rgb(255, 235, 0);
-              border-color: rgb(255, 235, 0);
-              border-top: 1px solid rgb(255, 235, 0);
-            }
+            
+          
 
             .buttonDer {
               border-radius: 0px 15px 15px 0px;
@@ -590,15 +789,7 @@ class Legumbre extends React.Component {
               touch-action: manipulation;
             }
 
-            .inputButtons {
-              max-width: 90px;
-              text-align: center;
-              border-width: thin;
-              border-left: none rgb(255, 235, 0);
-              border-right: none rgb(255, 235, 0);
-              border-color: rgb(255, 235, 0);
-              border-top: 1px solid rgb(255, 235, 0);
-            }
+            
 
             .buttonDer {
               border-radius: 0px 15px 15px 0px;
@@ -674,19 +865,26 @@ export async function getStaticProps({ params }) {
   const product = await res.json();
 
   let objectProp = {
-    _id: product._id,
-    name: product.name,
-    price: product.price,
-    description: product.description,
-    generalcategory: product.generalcategory,
-    imageUrl: product.imageUrl,
-    imageAlt: product.imageAlt,
-    type: product.type,
-    stock: product.stock,
-    urlRoute: product.urlRoute,
+            _id: product._id,
+            name: product.name,
+            price: product.price,
+            description: product.description,
+            category:product.category,
+            subcategory:product.subcategory,
+            generalcategory: product.generalcategory,
+            imageUrl: product.imageUrl,
+            imageAlt: product.imageAlt,
+            type: product.type,
+            stock: product.stock,
+            urlRoute: product.urlRoute,
+            imageName: product.imageName,
+            added: 0,
+            origin: product.origin,
+            minimunQty:product.minimunQty
   };
   // Pass post data to the page via props
   return { props: { objectProp } };
 }
+
 
 export default Legumbre;
