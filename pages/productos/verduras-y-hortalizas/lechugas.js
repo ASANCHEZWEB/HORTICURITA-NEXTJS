0eb05a17dd0de529032c1d7c24ff33de466b9df9
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-09-23 17:55:18 +0200

    terminada la seccion entera de verduras con details y todo

--- pages/productos/verduras-y-hortalizas/lechugas.js
+++ pages/productos/verduras-y-hortalizas/lechugas.js
@@ -1,15 +1,219 @@
-import React from "react";
+
+
+
+import React, { createElement } from "react";
 import Link from "next/link";
+import Head from "next/head";
+import axios from "axios";
+
+import { getLocalData } from "../../../components/getLocalStorage";
 
 class Lechugas extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
+    this.myVar = "";
+    this.myFunction = () => {
+      this.myVar = setTimeout(function () {
+        document.querySelector(".addCartContainer").innerHTML = "";
+        document.querySelector(".addCartContainer").style.display = "none";
+      }, 5000);
+    };
+    this.myStopFunction = () => {
+      clearTimeout(this.myVar);
+    };
 
+    this.state = {
+      // ARRAY DE LOCALSTORAGE
+      productInLS: [],
+      //ARRAY DE PRODUCTOS DE BASE DE DATOS
+      productProps: [...this.props.data],
+      screenSize: 0,
+    };
+  }
+  mostrarCuadro(product) {
+    let tipo = "";
+    if (product.type == "kilogramos") {
+      tipo = "0.5 Kg";
+    } else {
+      tipo = "1 Ud";
+    }
+    if (document.querySelector(".addCartContainer")) {
+      //asignamos html a la etiqueta
+      document.querySelector(".addCartContainer").innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Añadido ${tipo} de ${product.name} al carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    } else {
+      //asignamos html a la etiqueta
+      let elementDiv = document.createElement("div");
+      elementDiv.setAttribute("class", "addCartContainer");
+      elementDiv.innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Añadido ${tipo} de ${product.name} al carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector("body").appendChild(elementDiv);
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    }
+  }
+  
+  restarCuadro = (product) => {
+    let tipo = "";
+    if (product.type == "kilogramos") {
+      tipo = "0.5 Kg";
+    } else {
+      tipo = "1 Ud";
+    }
+    if (document.querySelector(".addCartContainer")) {
+      //asignamos html a la etiqueta
+      document.querySelector(".addCartContainer").innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Eliminado ${tipo} de ${product.name} del carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    } else {
+      //asignamos html a la etiqueta
+      let elementDiv = document.createElement("div");
+      elementDiv.setAttribute("class", "addCartContainer");
+      elementDiv.innerHTML = `
+      <img src="/141pxImages/${product.imageName}" alt="producto añadido" width="90px" height="90px">
+      <div style="height: 86%;width: 100%;display: flex;flex-direction: column;align-items: center;">
+      <span style="text-align: center;font-family: montserrat;font-size: smaller;"> Eliminado ${tipo} de ${product.name} del carrito</span>
+      <a style="color: white;" href="/carrito">VER CARRITO</a>
+      </div>`;
+      document.querySelector("body").appendChild(elementDiv);
+      document.querySelector(".addCartContainer").style.display = "";
+      //lo eliminamos
+      this.myStopFunction();
+      this.myFunction();
+    }
+  };
+  addNewProduct = (product) => {
+    let arrayLsCopy = [...this.state.productInLS];
+
+    if (product.stock == "si") {
+      if (product.added !== 0) {
+        //SI YA ESTA EN EL CARRO MODIFICAMOS EL QUE YA ESTA
+
+        arrayLsCopy.forEach((elLS) => {
+          if (elLS._id == product._id) {
+            elLS.added++;
+          }
+        });
+      } else {
+        product.added += 1;
+        arrayLsCopy.push(product);
+      }
+
+      this.setState({ productInLS: arrayLsCopy }, function () {
+        localStorage.setItem(
+          "cartProducts",
+          JSON.stringify(this.state.productInLS)
+        );
+        this.actualizarLS();
+      });
+      //aqui va la llamada ajax de que se ha metido al carro
+      axios.post("https://gestorhorticurita.herokuapp.com/api/addedCart/", {
+        id: product._id,
+      });
+      //aqui va la ejecución del cartel de carrito actualizado
+      this.mostrarCuadro(product);
+    }
+  };
+
+  restProduct = (product) => {
+    let arrayLsCopy = [...this.state.productInLS];
+    let arrayPropsCopy = [...this.state.productProps];
+
+    if (product.added == 1) {
+      //como es uno lo eliminamos del storage
+      arrayLsCopy.forEach(function (elem, index) {
+        if (elem._id == product._id) {
+          arrayLsCopy.splice(index, 1);
+          arrayPropsCopy.forEach((elProp) => {
+            if (elProp._id == product._id) {
+              elProp.added = 0;
+            }
+          });
+        }
+      });
+      this.restarCuadro(product);
+    }
+    if (product.added > 1) {
+      arrayLsCopy.forEach((elLS) => {
+        if (elLS._id == product._id) {
+          elLS.added--;
+        }
+      });
+      this.restarCuadro(product);
+    }
+
+    this.setState(
+      { productInLS: arrayLsCopy, productProps: arrayPropsCopy },
+      function () {
+        localStorage.setItem(
+          "cartProducts",
+          JSON.stringify(this.state.productInLS)
+        );
+        this.actualizarLS();
+      }
+    );
+  };
+
+  actualizarLS = () => {
+    this.setState({ productInLS: JSON.parse(getLocalData()) }, function () {
+      let arrayLsCopy = [...this.state.productInLS];
+      let arrayPropsCopy = [...this.state.productProps];
+      arrayLsCopy.forEach((elLS) => {
+        arrayPropsCopy.forEach((elProp) => {
+          if (elLS._id == elProp._id) {
+            elProp.added = elLS.added;
+          }
+        });
+      });
+
+      this.setState({ productProps: arrayPropsCopy });
+    });
+  };
+
+  componentDidMount() {
+    this.actualizarLS();
+
+    this.setState({ screenSize: 767 });
+  }
+componentWillUnmount(){
+  this.myStopFunction()
+  if(document.querySelector(".addCartContainer")){
+    document.querySelector(".addCartContainer").innerHTML = "";
+    document.querySelector(".addCartContainer").style.display = "none";
+}
+}
   render() {
     return (
       <>
+        <Head>
+          <title>🥬Lechugas🥬</title>
+          <meta
+            name="description"
+            content="Junto al tomate , la lechuga es clave en ensaladas, usala a tu gusto!😉"
+          ></meta>
+        </Head>
+
         <div className="metaDiv">
           <div>
             <h1>🥬Lechugas🥬</h1>
@@ -17,37 +221,52 @@ class Lechugas extends React.Component {
             Junto al tomate , la lechuga es clave en ensaladas, usala a tu gusto!😉
             </p>
             <p>
-              *Damos especial atención a la calidad de nuestros productos,por lo
+            *Damos especial atención a la calidad de nuestros productos,por lo
               tanto, si la fruta y verdura recibida está dañada ,
               <span>le devolveremos el dinero.</span>
             </p>
-            {/* <img
-              src="https://res.cloudinary.com/dfsni6m2x/image/upload/v1592682651/imagenes%20estaticas/frutas_y_verduras_joyikh.jpg"
-              alt="frutas y verduras"
-            /> */}
+
+            {/* <img src="/frutas_y_verduras.jpg" alt="frutas y verduras" /> */}
           </div>
         </div>
         <div className="container">
           {this.props.data.map((product, index) => {
             if (product.name) {
               return (
-                <div key={product._id}>
+                <div className="containerDivInfo" key={product._id}>
                   <Link
-                    href="/productos/verduras-y-hortalizas/lechugas/[id]"
-                    as={`/productos/verduras-y-hortalizas/lechugas/${product.urlRoute}`}
+                    href={`/productos/${product.category}/${product.subcategory}/[id]`}
+                    as={`/productos/${product.category}/${product.subcategory}/${product.urlRoute}`}
                   >
-                    <img src={product.imageUrl} alt={product.imageAlt} />
+                    <picture>
+                      <source
+                        media="(max-width: 767.98px)"
+                        srcSet={`/141pxImages/${product.imageName}`}/>
+                      <source
+                        media="(max-width: 1199.98px) and (min-width: 768px)"
+                        srcSet={`/174pxImages/${product.imageName}`}
+                      />
+                      <source
+                        media="(min-width: 1200px)"
+                        srcSet={`/270pxImages/${product.imageName}`}
+                      />
+                      <img
+                        className="imagenProduct"
+                        src={`/141pxImages/${product.imageName}`}
+                        alt={product.imageAlt}
+                      />
+                    </picture>
                   </Link>
 
                   <Link
-                    href="/productos/verduras-y-hortalizas/lechugas/[id]"
-                    as={`/productos/verduras-y-hortalizas/lechugas/${product.urlRoute}`}
+                  
+                    href={`/productos/${product.category}/${product.subcategory}/[id]`}
+                    as={`/productos/${product.category}/${product.subcategory}/${product.urlRoute}`}
                   >
                     <a>
                       <h2>{product.name}</h2>
                     </a>
                   </Link>
-
                   <span>
                     {product.price}€ /
                     {product.type === "kilogramos" ? "Kg" : "Ud"}
@@ -56,25 +275,38 @@ class Lechugas extends React.Component {
                     {product.stock === "si" ? (
                       <p>
                         <img
-                          src="https://res.cloudinary.com/dfsni6m2x/image/upload/v1593730373/iconosHorticurita/icono_disponible_a932xs.png"
+                          src={"/icono-stock-disponible.png"}
                           alt="icono disponible"
                         />
                         Disponible
                       </p>
                     ) : (
                       <p>
-                        <img
-                          src="https://res.cloudinary.com/dfsni6m2x/image/upload/v1593731819/iconosHorticurita/icono_sin_stock_ihhw8q.png"
-                          alt="icono sin stock"
-                        />
+                        <img src="/icono-sin-stock.png" alt="icono sin stock" />
                         No disponible
                       </p>
                     )}
                   </span>
                   <div>
-                    <button type="button">-</button>
-                    <input value="0" name="inputProduct" />
-                    <button type="button">+</button>
+                    <button
+                      type="button"
+                      onClick={() => this.restProduct(product)}
+                    >
+                      -
+                    </button>
+
+                    <p className="inputProducts">
+                      {product.type === "kilogramos"
+                        ? this.state.productProps[index].added / 2
+                        : this.state.productProps[index].added}
+                    </p>
+
+                    <button
+                      type="button"
+                      onClick={() => this.addNewProduct(product)}
+                    >
+                      +
+                    </button>
                   </div>
                 </div>
               );
@@ -82,6 +314,9 @@ class Lechugas extends React.Component {
           })}
         </div>
         <style jsx>{`
+          .buttonCart > img {
+            object-fit: contain;
+          }
           .metaDiv {
             max-width: 768px;
             margin: 0 auto;
@@ -92,7 +327,18 @@ class Lechugas extends React.Component {
             padding-left: 25px;
             padding-right: 25px;
           }
-
+          .inputProducts {
+            min-width: 34px;
+            text-align: center;
+            margin: 0px;
+            padding: 6px;
+            border-width: thin;
+            border-left: none;
+            border-right: none;
+            border-color: #ffeb00;
+            border-top: 1px solid #ffeb00;
+            border-bottom: 1px solid #ffeb00;
+          }
           .container > div > span {
             font-family: montserrat;
             font-size: 13px;
@@ -106,6 +352,7 @@ class Lechugas extends React.Component {
           }
           .container > div > span > p > img {
             width: 20px;
+            object-fit: contain;
           }
           .metaDiv > div > h1 {
             text-align: center;
@@ -130,16 +377,6 @@ class Lechugas extends React.Component {
             height: 28px;
             margin-top: 15px;
             margin-bottom: 15px;
-          }
-
-          .container > div > div > input {
-            max-width: 50px;
-            text-align: center;
-            border-width: thin;
-            border-left: none;
-            border-right: none;
-            border-color: #ffeb00;
-            border-top: 1px solid #ffeb00;
           }
 
           .container > div > a > h2 {
@@ -175,12 +412,14 @@ class Lechugas extends React.Component {
               justify-content: center;
             }
 
-            .container > div > img {
-              width: 100%;
+            .imagenProduct {
               cursor: pointer;
+              object-fit: contain;
+              width: 100%;
+              height: auto;
             }
 
-            .container > div {
+            .containerDivInfo {
               width: 142px;
               text-align: center;
               margin-left: 1%;
@@ -203,12 +442,14 @@ class Lechugas extends React.Component {
               justify-content: center;
             }
 
-            .container > div > img {
-              width: 100%;
+            .imagenProduct {
               cursor: pointer;
+              object-fit: contain;
+              width: 100%;
+              height: auto;
             }
 
-            .container > div {
+            .containerDivInfo {
               width: 169px;
               text-align: center;
               margin-left: 2%;
@@ -229,15 +470,17 @@ class Lechugas extends React.Component {
               justify-content: center;
             }
 
-            .container > div > img {
-              width: 100%;
+            .imagenProduct {
               cursor: pointer;
+              object-fit: contain;
+              width: 100%;
+              height: auto;
             }
             .container > div > span {
               font-family: montserrat;
               font-size: 14px;
             }
-            .container > div {
+            .containerDivInfo {
               width: 175px;
               text-align: center;
               margin-left: 2%;
@@ -258,15 +501,17 @@ class Lechugas extends React.Component {
               justify-content: center;
             }
 
-            .container > div > img {
-              width: 100%;
+            .imagenProduct {
               cursor: pointer;
+              object-fit: contain;
+              width: 100%;
+              height: auto;
             }
             .container > div > span {
               font-family: montserrat;
               font-size: 14px;
             }
-            .container > div {
+            .containerDivInfo {
               width: 225px;
               text-align: center;
               margin-left: 2%;
@@ -288,16 +533,29 @@ class Lechugas extends React.Component {
               max-width: 1325px;
               margin: 0 auto;
             }
-
-            .container > div > img {
-              width: 100%;
+            .inputProducts {
+              min-width: 53px;
+              text-align: center;
+              margin: 0px;
+              padding: 6px;
+              border-width: thin;
+              border-left: none;
+              border-right: none;
+              border-color: #ffeb00;
+              border-top: 1px solid #ffeb00;
+              border-bottom: 1px solid #ffeb00;
+            }
+            .imagenProduct {
               cursor: pointer;
+              object-fit: contain;
+              width: 100%;
+              height: auto;
             }
             .container > div > span {
               font-family: montserrat;
               font-size: 14px;
             }
-            .container > div {
+            .containerDivInfo {
               width: 271px;
               text-align: center;
               margin-left: 2%;
@@ -331,12 +589,18 @@ export async function getStaticProps() {
             name: product.name,
             price: product.price,
             description: product.description,
+            category:product.category,
+            subcategory:product.subcategory,
             generalcategory: product.generalcategory,
+            subcategoryPos: product.subcategoryPos,
             imageUrl: product.imageUrl,
             imageAlt: product.imageAlt,
             type: product.type,
             stock: product.stock,
             urlRoute: product.urlRoute,
+            imageName: product.imageName,
+            added: 0,
+            minimunQty:product.minimunQty
           };
         } else {
           return {};
